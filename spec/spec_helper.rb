2bfdde95d35b75ed0f739b316fa3ee2e2cdc874a
Steve Tuckner <steve@open-orgsolutions.com>
2014-03-30 20:23:13 -0500

    Allowed this to run with PhantomJS (but now running with actual active record needs to uncomment some lines).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 
 if ENV['run_with_real_active_record']
-  Bundler.require(:test)
-  require "active_record"
+  #Bundler.require(:test)
+  #require "active_record"
 else
   if RUBY_ENGINE == "opal"
     require 'opal-rspec'
