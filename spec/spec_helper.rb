6006f3614029c40e3895b431e1b1e79adbaadc56
Steve Tuckner <steve@open-orgsolutions.com>
2014-05-03 16:20:29 -0500

    add commentary to how to run specs in diff ways

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,18 @@
-
 if ENV['run_with_real_active_record']
-  #Bundler.require(:test)
-  #require "active_record"
+  puts "running with real active record"
+  # uncomment only when running from command line: run_with_real_active_record=true rspec spec
+  # Bundler.require(:test)
+  # require "active_record"
 else
   if RUBY_ENGINE == "opal"
+    puts "running with Opal"
     require 'opal-rspec'
     require 'opal-activerecord'
   else
+    puts "running with MRI"
     require_relative '../opal/active_record/core'
+    # uncomment only when running  in MRI
+    # require 'json'
   end
 end
 
