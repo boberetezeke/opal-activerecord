b82cad82c72f27f1ef12d8c54d4591664ede2ced
Steve Tuckner <stevetuckner@gmail.com>
2015-05-20 16:00:22 -0500

    Made changes to issues found when running in actual app

--- opal/active_record/store/memory_store.rb
+++ opal/active_record/store/memory_store.rb
@@ -15,6 +15,7 @@ module ActiveRecord
     end
 
     def execute(select_manager)
+      init_new_table(select_manager.table_name)
       table =  @tables[select_manager.table_name.to_s].values.map{|value| {select_manager.table_name => value} }
       debug "MemoryStore#execute: table name = #{select_manager.table_name}"
       debug "MemoryStore#execute: tables = #{@tables.keys}"
@@ -29,12 +30,12 @@ module ActiveRecord
 
     def push(table_name, record)
       init_new_table(table_name)
-      @tables[table_name][record.id] = record
+      store_record_on_table(@tables[table_name], record.id, record)
     end
 
     def find(klass, table_name, id)
       if @tables[table_name]
-        record = @tables[table_name][id]
+        record = get_record_from_table(@tables[table_name], id)
       else
         record = nil
       end
@@ -42,39 +43,42 @@ module ActiveRecord
       if record
         klass.new(record)
       else
-        raise ActiveRecord::RecordNotFound.new("Record not found: class #{klass}, id #{id}") unless record
+        raise ActiveRecord::RecordNotFound.new("Record not found: class #{klass}, id #{id.inspect}") unless record
       end
     end
 
     def create(klass, table_name, record, options={})
-      debug "MemoryStore#Create(#{record})"
+      debug "MemoryStore#create(#{record})"
       init_new_table(table_name)
       next_id = gen_next_id(table_name)
-      @tables[table_name][next_id] = record.attributes.dup.merge({"id" =>  next_id})
+      store_record_on_table(@tables[table_name], next_id, record.attributes.dup.merge({"id" =>  next_id}))
       notify_observers(:insert, record, options)
       return next_id
     end
 
     def update(klass, table_name, record, options={})
+      debug "MemoryStore#update(#{record})"
       init_new_table(table_name)
       table = @tables[table_name]
-      old_attributes = table[record.id]
+      old_attributes = get_record_from_table(table, record.id)
+      store_record_on_table(table, record.id, record.attributes.dup)
       if old_attributes
         notify_observers(:update, record, options) if record.attributes != old_attributes
+      else
+        notify_observers(:insert, record, options)
       end
-      table[record.id] = record.attributes.dup
     end
 
     def update_id(klass, table_name, old_id, new_id, options={})
       table = @tables[table_name]
-      record = table.delete(old_id)
+      record = remove_record_from_table(table, old_id)
       record[:id] = new_id
-      table[new_id] = record
+      store_record_on_table(table, new_id, record)
     end
 
     def destroy(klass, table_name, record, options={})
       notify_observers(:delete, record, options)
-      @tables[table_name].delete(record.id)
+      remove_record_from_table(@tables[table_name], record.id)
     end
 
     def to_s
@@ -91,6 +95,8 @@ module ActiveRecord
       @tables[table_name]
     end
 
+    private
+
     def gen_next_id(table_name)
       next_id = @next_ids[table_name]
       @next_ids[table_name] += 1
@@ -102,5 +108,16 @@ module ActiveRecord
       @next_ids[table_name] ||= 1
     end
 
+    def store_record_on_table(table, id, record)
+      table[id.to_s] = record
+    end
+
+    def get_record_from_table(table, id)
+      table[id.to_s]
+    end
+
+    def remove_record_from_table(table, id)
+      table.delete(id.to_s)
+    end
   end
 end
